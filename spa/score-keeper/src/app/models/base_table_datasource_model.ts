e11a31fef6ec63062f114289b4e3509c037cbef5
tspDevelopment <pieter.contrec@gmail.com>
2025-03-15 16:55:27 -0500

    Add base table data source model and base model interfaces; implement pagination, sorting, filtering, and CRUD operations for data handling in the application.

---
+++ spa/score-keeper/src/app/models/base_table_datasource_model.ts
@@ -0,0 +1,155 @@
+import { Observable } from "rxjs";
+import { BaseDataSource } from "../components/table/table.component";
+import { AbstractDataService } from "../services/abstract-data.service";
+import { LoggerService } from "../services/logger.service";
+import { BaseModel } from "./models";
+
+export abstract class BaseTableDataSourceModel<T extends BaseModel> extends BaseDataSource<T> {
+
+    public DEFAULT_PAGE_SIZE = 100;
+    public DEFAULT_PAGE = 0;
+
+    constructor(
+        private service: AbstractDataService<T>, 
+        public logger: LoggerService
+    ) {
+        super();
+    }
+
+    override connect(): Observable<T[]> {
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          pageSize = this.paginator.pageSize
+        }
+        this.service.query('', this.DEFAULT_PAGE, pageSize, '', '').subscribe({
+          next: (data) => {
+            this.observable.next(data);
+          },
+          error: (error) => {
+            throw new Error(`Error loading ${this.service.root_api}: ${error}`);
+          }
+        });
+        return this.observable;
+      }
+    
+      override disconnect(): void {
+        this.observable.complete();
+      }
+      
+      override filter(filterValue: string): void {
+        const page = this.DEFAULT_PAGE;
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          pageSize = this.paginator.pageSize;
+        }
+        let sortActive = '';
+        let sortDirection = '';
+        if (this.sorter) {
+          sortActive = this.sorter.active;
+          sortDirection = this.sorter.direction;
+        }
+        this.service.query(filterValue, page, pageSize, sortActive, sortDirection).subscribe({
+          next: (data) => {
+            this.observable.next(data);
+          },
+          error: (error) => {
+            throw new Error(`Error filtering ${this.service.root_api}: ${error}`);
+          }
+        });
+      }
+      
+      override sortAction(sort: { active: string; direction: string; }): void {
+        let page = this.DEFAULT_PAGE;
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          page = this.paginator.pageIndex;
+          pageSize = this.paginator.pageSize;
+        }
+        this.logger.debug(`Sort roles: ${sort.active} ${sort.direction}`);
+        this.service.query('', page, pageSize, sort.active, sort.direction).subscribe({
+          next: (data) => {
+            this.observable.next(data);
+          },
+          error: (error) => {
+            throw new Error(`Error sorting ${this.service.root_api}: ${error}`);
+          }
+        });
+      }
+      
+      override setPage(page: number, pageSize: number): void {
+        let sortActive = '';
+        let sortDirection = '';
+        if (this.sorter) {
+          sortActive = this.sorter.active;
+          sortDirection = this.sorter.direction;
+        }
+        this.service.query('', page, pageSize, sortActive, sortDirection).subscribe({
+          next: (data) => {
+            this.observable.next(data);
+          },
+          error: (error) => {
+            throw new Error(`Error getting page ${page} for ${this.service.root_api}: ${error}`);
+          }
+        });
+      }
+      
+      override addRow(row: T): void {
+        let page = this.DEFAULT_PAGE;
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          page = this.paginator.pageIndex;
+          pageSize = this.paginator.pageSize;
+        }
+        this.service.create(row).subscribe({
+          next: () => {
+            this.setPage(page, pageSize);
+          },
+          error: (error) => {
+            throw new Error(`Error adding role: ${error}`);
+          }
+        });
+      }
+      
+      override deleteRow(row: T): void {
+        let page = this.DEFAULT_PAGE;
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          page = this.paginator.pageIndex;
+          pageSize = this.paginator.pageSize;
+        }
+        if (!row.id) {
+          return;
+        }
+        this.service.delete(row.id).subscribe({
+          next: () => {
+            this.setPage(page, pageSize);
+          },
+          error: (error) => {
+            throw new Error(`Error deleting role: ${error}`);
+          }
+        });
+      }
+      
+      override updateRow(row: T): void {
+        let page = this.DEFAULT_PAGE;
+        let pageSize = this.DEFAULT_PAGE_SIZE;
+        if (this.paginator) {
+          page = this.paginator.pageIndex;
+          pageSize = this.paginator.pageSize;
+        }
+        this.service.update(row).subscribe({
+          next: () => {
+            this.setPage(page, pageSize);
+          },
+          error: (error) => {
+            throw new Error(`Error updating role: ${error}`);
+          }
+        });
+      }
+      
+      override async getTotalItemCount(): Promise<number> {
+        const count = await this.service.getTotalItemCount().toPromise();
+        return count ?? 0;
+      }
+
+}
---
+++ spa/score-keeper/src/app/models/models.ts
@@ -0,0 +1,96 @@
+
+export interface BaseModel {
+    id?: string;
+    created_at?: string;
+    updated_at?: string;
+}
+
+export interface Priviledge extends BaseModel {
+    priviledge_name: string;
+  }
+
+export interface Role extends BaseModel {
+    role_name: string;
+    priviledges: Priviledge[];
+}
+
+export interface User extends BaseModel {
+    username?: string;
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    role?: Role;
+    account_locked?: boolean;
+    last_login?: string;
+}
+
+export interface Grade extends BaseModel {
+    grade_name: string;
+}
+
+export interface StudentGroup extends BaseModel {
+    group_name: string;
+}
+
+export interface Student extends BaseModel {
+    firstname: string;
+    lastname: string;
+    grade: Grade;
+    student_group: StudentGroup;
+}
+
+export interface Interval {
+    repeat: 'daily' | 'weekly' | 'monthly' | 'none';
+    month_day: number;
+    week_day: number;
+    hour: number;
+    minute: number;
+}
+
+export interface PointCategory extends BaseModel {
+    category_name: string;
+    description: string;
+    deleted: boolean;
+}
+
+export interface Point extends BaseModel {
+    points: number;
+    student_group: StudentGroup;
+    interval: Interval;
+    point_category: PointCategory;
+    deleted: boolean;
+}
+
+export interface Event extends BaseModel {
+    event_name: string;
+    interval: Interval;
+    deleted: boolean;
+    student_groups: StudentGroup[];
+    point_categories: PointCategory[];
+}
+
+export interface EventInstance extends BaseModel {
+    event: Event;
+    event_date: string;
+    completed: boolean;
+    deleted: boolean;
+}
+
+export interface PointEarned extends BaseModel {
+    student: Student;
+    event_instance: EventInstance;
+    points: Point;
+    deleted: boolean;
+}
+
+export interface PointSpent extends BaseModel {
+    student: Student;
+    event_instance: EventInstance;
+    points: number;
+    deleted: boolean;
+}
+
+export interface RunningTotal extends BaseModel {
+    student: Student;
+    total_points: number;
+}
